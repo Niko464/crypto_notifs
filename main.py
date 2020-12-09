e1521ac963d2377af121fc1e9663819ad77a56eb
De Smet Nikolaj <nikolaj.de-smet@epitech.eu>
2020-12-09 22:57:40 +0100

    Updating --> main.py (Trying to fix the real records for 24h)

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ from binance.exceptions import *
 global crypto_list
 global bsm
 global client
+global is_ready_to_receive_new_msg
 crypto_list = {}
 client = Client(private_config.API_KEY, private_config.API_SECRET)
+is_ready_to_receive_new_msg = True
 
 def load_config(file):
 	try:
@@ -68,7 +70,11 @@ def start_data_streams():
 def received_price_msg(msg):
 	try:
 		if msg['e'] != 'error':
-			received_price(msg['s'], msg['k']['t'], float(msg['k']['c']))
+			global is_ready_to_receive_new_msg
+			if is_ready_to_receive_new_msg == True:
+				is_ready_to_receive_new_msg = False
+				received_price(msg['s'], msg['k']['t'], float(msg['k']['c']))
+				is_ready_to_receive_new_msg = True
 		else:
 			comm.report_error(msg="Error 'B' with the price getter info...",
 				mail=False, console=True, log=True, telegram=True, quit=False)
@@ -77,6 +83,7 @@ def received_price_msg(msg):
 			mail=False, console=True, log=True, telegram=True, quit=False)
 
 def received_price(symbol, timestamp, price):
+	#TODO MODIFY SO THAT THE 24h LOW HIGH IS CORRECT BECAUSE NOW IT IS AN ALL TIME LOW HIGH 
 	crypto = crypto_list[symbol]
 	if timestamp in crypto["prices"]:
 		crypto["prices"][timestamp] = price
@@ -84,8 +91,19 @@ def received_price(symbol, timestamp, price):
 		comm.communicate(msg=str(symbol) + " | Updating " + str(timestamp) + ", price: " + str(price),
 			mail=False, console=True, telegram=False, log=True)
 		crypto["prices"][int(timestamp)] = price
-		if (len(crypto["prices"]) > 60):
-			crypto["prices"].pop(timestamp - 3600000) # 3600000 is 1 hour in milliseconds
+		if (len(crypto["prices"]) > 1440): # There are 1440 minutes in a day
+			crypto["prices"].pop(timestamp - (3600000 * 24)) # 3600000 is 1 hour in milliseconds
+	# Checking if current records are outdated
+	if (crypto["max_1h"][0] + 3600000 < timestamp):
+		crypto["max_1h"] = find_new_record(timestamp, '1h', crypto["prices"], 'High')
+	if (crypto["min_1h"][0] + 3600000 < timestamp):
+		crypto["min_1h"] = find_new_record(timestamp, '1h', crypto["prices"], 'Low')
+	if (crypto["max_24h"][0] + (3600000 * 24) < timestamp):
+		crypto["max_24h"] = find_new_record(timestamp, '24h', crypto["prices"], 'High')
+	if (crypto["min_24h"][0] + (3600000 * 24) < timestamp):
+		crypto["min_24h"] = find_new_record(timestamp, '24h', crypto["prices"], 'Low')
+	
+	# Checking if current price is a record
 	if (price > crypto["max_1h"][1]):
 		crypto["max_1h"] = (int(timestamp), price)
 		if (price > crypto["max_24h"][1]):
@@ -108,6 +126,23 @@ def new_record(symbol, timeframe, record_type, price, crypto):
 	if (diff / price >= 0.002): #0.002 is for 0.2% difference this could be a variable but flemme
 		comm.communicate(msg=f"{symbol} --> {timeframe} New {record_type}: {price}", mail=False, console=True, telegram=True, log=True)
 		crypto["last_notif_price"] = price
+
+def find_new_record(timestamp, timeframe, prices, record_type):
+	try:
+		record = (0, sys.maxsize) if record_type == 'Low' else (0, -sys.maxsize - 1)
+		
+		for i in range(0, (60 if timeframe == '1h' else 1440)):
+			if (record_type == 'Low'):
+				if prices[timestamp - (i * 60000)] < record[1]:
+					record = (timestamp - (i * 60000), prices[timestamp - (i * 60000)])
+			else:
+				if prices[timestamp - (i * 60000)] > record[1]:
+					record = (timestamp - (i * 60000), prices[timestamp - (i * 60000)])
+		
+		comm.communicate(msg="TEST timestamp: " + str(timestamp) + " record_type: " + record_type + " time_frame: " + timeframe + " New record found: " + str(record), mail=False, console=False, telegram=True, log=True)
+		return record
+	except Exception as e:
+		print("Caught exception: " + str(e))
 
 def main(config_file = "configs/default.json"):
 	if not (os.path.exists(config.LOG_DIRECTORY)):
